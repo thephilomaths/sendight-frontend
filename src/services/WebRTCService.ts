e8dbb4a19e807c0638f29bb81bd47cd013ee23fb
Kartik Behl <kartikbehl99@gmail.com>
2021-05-17 14:08:43 +0530

    Refactoring

--- src/services/WebRTCService.ts
+++ src/services/WebRTCService.ts
@@ -1,11 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
-import {webRTCConnectionInfo} from '../config';
-import {WebRTCEventsHandler} from '../handlers/WebRTCEventsHandler';
+import { webRTCConnectionInfo } from '../config';
+import { WebRTCEventsHandler } from '../handlers/WebRTCEventsHandler';
 
 interface ICreateDataChannelParams {
   label: string;
-  onOpenHandler?: (event: any) => void;
-  onMessageHandler?: (event: any) => void;
+  onOpenHandler?: (event: Event | RTCDataChannelEvent) => void;
+  onMessageHandler?: (event: Event | RTCDataChannelEvent) => void;
 }
 
 class WebRTCServiceClass {
@@ -14,9 +14,15 @@ class WebRTCServiceClass {
   dataChannel: RTCDataChannel;
   fileToMetadataMap: { [key: string]: Record<string, string> };
 
-  createDataChannel = ({ label, onOpenHandler, onMessageHandler }: ICreateDataChannelParams): void => {
-    this.webRTCConnection.onicecandidate = WebRTCEventsHandler.handleICECandidateEvent;
-    this.webRTCConnection.onconnectionstatechange = WebRTCEventsHandler.handleConnectionStateChange;
+  createDataChannel = ({
+    label,
+    onOpenHandler,
+    onMessageHandler,
+  }: ICreateDataChannelParams): void => {
+    this.webRTCConnection.onicecandidate =
+      WebRTCEventsHandler.handleICECandidateEvent;
+    this.webRTCConnection.onconnectionstatechange =
+      WebRTCEventsHandler.handleConnectionStateChange;
 
     this.dataChannel = this.webRTCConnection.createDataChannel(label);
 
@@ -27,31 +33,43 @@ class WebRTCServiceClass {
     if (onMessageHandler) {
       this.dataChannel.onmessage = onMessageHandler;
     }
-  }
+  };
 
   createNewWebRTCClient = (): RTCPeerConnection => {
     this.webRTCConnection = new RTCPeerConnection(webRTCConnectionInfo);
     return this.webRTCConnection;
-  }
+  };
+
+  closeWebRTCClient = (): void => {
+    if (this.webRTCConnection.connectionState !== 'closed') {
+      this.webRTCConnection.close();
+    }
+  };
 
   sendMetadata = (files: File[]): void => {
-    const metadata = files.reduce((accumulator: {[key: string]: Record<string, string | number>}, file) => {
-      const uuid = uuidv4();
-      const { name, type, size } = file;
+    const metadata = files.reduce(
+      (
+        accumulator: { [key: string]: Record<string, string | number> },
+        file
+      ) => {
+        const uuid = uuidv4();
+        const { name, type, size } = file;
 
-      accumulator[uuid] = { name, type, size };
+        accumulator[uuid] = { name, type, size };
 
-      return accumulator;
-    }, {});
+        return accumulator;
+      },
+      {}
+    );
 
     this.createDataChannel({ label: 'metadataChannel' });
 
     this.dataChannel.send(JSON.stringify(metadata));
-  }
+  };
 
-  // sendFile = (file: File, label: string): void => {
-  //   this.createDataChannel(label);
-  // }
+  sendFile = (file: File, label: string): void => {
+    this.createDataChannel({ label });
+  };
 }
 
 export const WebRTCService = new WebRTCServiceClass();
