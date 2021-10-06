a319c11e797d576d2bf6d096852e12e04b401298
Deepanshu Rana <deepanshurana165@gmail.com>
2021-10-06 09:13:14 +0530

    Added stun servers

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+const webRTCConnectionInfo = {
+  iceServers: [
+    {
+      urls: [
+        'stun:stun.stunprotocol.org',
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302'
+      ],
+    },
+  ],
+};
+
+export { webRTCConnectionInfo };
